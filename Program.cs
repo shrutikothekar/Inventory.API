7cef49ba93e18115cb1bf77b20535a904cf1a187
shrutikothekar <shrutikothekar01@gmail.com>
2026-01-13 23:15:54 +0530

    test

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();//test
 
 //services
 // Register the interface -> implementation so the type is resolved by DI
